ef7bc319107f01e2915ca7a600cf806f2dbcfc32
Arghyadip Chakraborty <arghyadip.chak16@gmail.com>
2021-11-03 09:31:05 +0530

    Autoupdate

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git pull
+
+if [ -n "$(git status --porcelain)" ]; then
+  read -p "Commit Message: " commit_msg
+  git add --all
+  git commit -m $commit_msg
+  git push
+else
+  echo "Nothing to be pushed!";
+fi
